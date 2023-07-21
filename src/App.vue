3d30ed333bc61db837abd928aecc2bae578014b1
Tapan Padaliya <tapan@7span.com>
2023-07-21 19:27:15 +0530

    init const

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,59 @@
 <template>
   <div>
-    <button class="button" @click="logInWithFacebook">
-      Login with Facebook
-    </button>
+    <div v-if="userInfo">
+      <p>User ID: {{ userInfo.id }}</p>
+      <p>Name: {{ userInfo.name }}</p>
+      <p>Email: {{ userInfo.email }}</p>
+      <p>First Name: {{ userInfo.first_name }}</p>
+      <p>Last Name: {{ userInfo.last_name }}</p>
+    </div>
+    <button @click="getUserInfo">Get User Info</button>
   </div>
 </template>
 <script>
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: "290647546838436",
+    cookie: true,
+    xfbml: true,
+    version: "v17.0",
+  });
+
+  FB.AppEvents.logPageView();
+};
+
+(function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "facebook-jssdk");
+
 export default {
-  name: "facebookLogin",
+  data() {
+    return {
+      userInfo: null,
+    };
+  },
   methods: {
-    async logInWithFacebook() {
-      await this.loadFacebookSDK(document, "script", "facebook-jssdk");
-      await this.initFacebook();
-      // window.FB.login(function (response) {
-      //   if (response.authResponse) {
-      //     alert("You are logged in &amp; cookie set!");
-      //     console.log("Response : " + JSON.stringify(response));
-      //     console.log("Response : " + response.data);
-      //     // Now you can redirect the user or do an AJAX request to
-      //     // a PHP script that grabs the signed request from the cookie.
-      //   } else {
-      //     alert("User cancelled login or did not fully authorize.");
-      //   }
-      // });
-      window.FB.api("/me", "GET", { fields: "id,name" }, function (response) {
-        console.log(response);
-      });
-      return false;
-    },
-    async initFacebook() {
-      window.fbAsyncInit = function () {
-        window.FB.init({
-          appId: "290647546838436", //You will need to change this
-          cookie: true, // This is important, it's not enabled by default
-          version: "v17.0",
-        });
-      };
-    },
-    async loadFacebookSDK(d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
+    getUserInfo() {
+      FB.api(
+        "/me",
+        { fields: "id,name,email,first_name,last_name" },
+        (response) => {
+          if (response.error) {
+            console.log(response.error);
+          } else {
+            this.userInfo = response;
+          }
+        }
+      );
     },
   },
 };
 </script>
-<style>
-.button {
-  color: white;
-  min-width: 150px;
-  background-color: #000000a1;
-  height: 2.5rem;
-  border-radius: 2rem;
-  font-weight: 400;
-  font-size: 0.8rem;
-}
-</style>
